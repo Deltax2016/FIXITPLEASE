4e4a40b216291deba02d64ccd8e006e91ab9e7ad
Deltax2016 <deltaxussr@gmail.com>
2016-10-01 15:03:41 +0400

    Add files via upload

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -2,19 +2,21 @@
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 stringstream s3;
-string s1,s2 = "9223372036954775807",s5 = "9223372036954775808";
+string s1,s4, s2 = "9223372036954775807";
 long long n;
+string s5 = "9223372036954775808";
 bool f1 = 0;
 
 
 int main()
 {
 	cin >> s1;
-	if ((s1[0] != '-') && (s1.length() < 20) && (s1 <= s2))
+	if ((s1[0] != '-') && (s1.length() < 20) && (((s1 <= s2) && (s1.length() <= s2.length())) || (s2.length() > s1.length())))
 	{
 			s3 << s1;
 			s3 >> n;
@@ -42,7 +44,7 @@ int main()
 	if (s1[0] == '-')
 	{
 		s1 = s1.substr(1, s1.length()-1);
-		if ((s1.length() < 20) && (s1 <= s5))
+		if ((s1.length() < 20) && (((s1 <= s2) && (s1.length() <= s5.length())) || (s5.length() > s1.length())))
 		{
 			if (s1 == "9223372036954775808")
 			{
@@ -52,6 +54,7 @@ int main()
 			{
 				s3 << s1;
 				s3 >> n;
+				cout << n << endl;
 				if (n <= 128)
 				{
 					cout << "byte";
@@ -75,13 +78,10 @@ int main()
 			}
 		}
 	}
-	cout << n << endl;
 	if (f1 == 0)
 	{
 		cout << "BigInteger";
 	}
-	system("pause");
-	
 	return 0;
 }
 
